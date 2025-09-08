8f4799d8b450f9e821b4be71c607462d33098f0a
umairprodev <mumairashrafofficial@gmail.com>
2025-09-08 23:33:39 +0500

    uswah saeed portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const text =
+  "✦ Exergames ✦ Rapid Prototyping ✦ Game Feel/UX ✦ Generative AI ✦ Navigation ✦ Geospatial Visualization ✦ Data Analytics ✦ AR ✦ VR ✦ Computer Vision ✦ Technical Art ✦ Web Development ✦ Micro-Controllers ";
+const container = document.getElementById("waveText");
+
+const makeWaveSpans = (str, offset = 0) => {
+  const frag = document.createDocumentFragment();
+  for (let i = 0; i < str.length; i++) {
+    const span = document.createElement("span");
+    span.textContent = str[i];
+    span.className = "char";
+    // negative delay starts animation already "in motion"
+    span.style.animationDelay = `${-(i + offset) * 0.05}s`;
+    frag.appendChild(span);
+  }
+  return frag;
+};
+
+
+
+function showMobileNav() {
+      const nav = document.getElementById("nav");
+      if (nav.classList.contains("hidden")) {
+        nav.classList.remove("hidden");
+      } else {
+        nav.classList.add("hidden");
+      }
+    }
